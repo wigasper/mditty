f2f7b127b06b1153d4cc7f88ddd358cbfec813a3
William Gasper <wkg@williamgasper.com>
2020-06-14 14:39:20 -0500

    added some tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,12 +4,29 @@ pub mod utils;
 mod tests {
     pub use crate::utils::*;
 
-    use std::collections::{HashMap, HashSet};
     use std::path::{Path, PathBuf};
 
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_get_file_extension_0() {
+        assert_eq!(get_file_extension(&PathBuf::from("test.rs")), "rs");
+    }
+
+    #[test]
+    fn test_get_file_extension_1() {
+        assert_eq!(get_file_extension(&PathBuf::from("test")), "");
+    }
+
+    #[test]
+    fn test_get_output_path_0() {
+        assert_eq!(
+            get_output_path(&PathBuf::from("test.rs")),
+            PathBuf::from("test.md")
+        );
     }
 
     #[test]
